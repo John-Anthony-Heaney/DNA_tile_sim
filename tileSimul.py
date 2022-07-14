44bcd51c8c31cf0f40844167c5f1479aa52ca203
John-Anthony-Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2022-07-14 20:04:11 +0100

    Add files via upload

---
+++ tileSimul.py
@@ -0,0 +1,210 @@
+import pygame
+import random
+import copy
+
+class tile :
+    def __init__(self, north, east,south,west):
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+
+    def setTilePos(self,x,y):
+        self.x = x
+        self.y = y
+
+def showTable(size,screenDim):
+
+    squareDim = screenDim/(size+2)
+    tableDim = screenDim - squareDim
+    for x in range(1,size + 2):
+        pygame.draw.line(screen, (0,0,0), (x*squareDim,squareDim), (x*squareDim,tableDim),1)
+        pygame.draw.line(screen, (0,0,0), (squareDim,x*squareDim), (tableDim,x*squareDim),1)
+
+def showTile(tile,screenDim,tableSize):
+
+        size = (screenDim/(tableSize+2))/2
+
+        x = size*3 + size *2*tile.x
+        y = screenDim-size*3 - size*2*tile.y
+
+        tileColors = {0:(255,222,173),1:(0,255,0),2:(255,0,0)}
+
+        brown = (255,222,173)
+        green = (0,255,0)
+        red = (255,0,0)
+        black = (0,0,0)
+
+        center = (x,y)
+        topLeft = (x-size,y-size)
+        topRight = (x+size,y-size)
+        bottomLeft = (x-size,y+size)
+        bottomRight = (x+size,y+size)
+
+        pygame.draw.polygon(screen, tileColors[tile.north], (topLeft,topRight,center))
+        pygame.draw.polygon(screen, tileColors[tile.east], (bottomRight,topRight,center))
+        pygame.draw.polygon(screen, tileColors[tile.south] ,(bottomLeft,bottomRight,center))
+        pygame.draw.polygon(screen, tileColors[tile.west], (topLeft,bottomLeft,center))
+
+        pygame.draw.line(screen, black, topLeft, topRight,2)
+        pygame.draw.line(screen, black, bottomLeft, bottomRight,2)
+        pygame.draw.line(screen, black, topLeft, bottomLeft,2)
+        pygame.draw.line(screen, black, topRight, bottomRight,2)
+        pygame.draw.line(screen, black, topLeft, bottomRight,2)
+        pygame.draw.line(screen, black, bottomLeft, topRight,2)
+
+        font = pygame.font.Font('freesansbold.ttf', int(size/3))
+
+        textNorth = font.render(str(tile.north), False, black, tileColors[tile.north])
+        textEast = font.render(str(tile.east), False, black, tileColors[tile.east])
+        textSouth = font.render(str(tile.south), False, black, tileColors[tile.south])
+        textWest = font.render(str(tile.west), False, black, tileColors[tile.west])
+
+        textRectNorth = textNorth.get_rect()
+        textRectEast = textEast.get_rect()
+        textRectSouth = textSouth.get_rect()
+        textRectWest = textWest.get_rect()
+
+        textRectNorth.center = (x, y-size/2)
+        textRectEast.center =  (x+size/2, y)
+        textRectSouth.center = (x, y+size/2)
+        textRectWest.center = (x-size/2, y)
+
+
+        screen.blit(textNorth, textRectNorth)
+        screen.blit(textEast, textRectEast)
+        screen.blit(textSouth, textRectSouth)
+        screen.blit(textWest, textRectWest)
+
+def checkTile(pos):
+        returnTile = None
+        numPosTile = 0
+        x = pos[0]
+        y = pos[1]
+        posList = [(x,y+1),(x+1,y),(x,y-1),(x-1,y)]
+        for tile in tileSet:
+            numTrue = 0
+            numFalse = 0
+            if(allTiles.get(posList[1]) != None):
+                if(tile.east == allTiles.get(posList[1]).west):
+                    numTrue +=1
+                else:
+                    numFalse +=1
+
+            if(allTiles.get(posList[3]) != None):
+                if(tile.west == allTiles.get(posList[3]).east):
+                    numTrue +=1
+                else:
+                    numFalse +=1
+
+            if(allTiles.get(posList[0]) != None):
+                if(tile.north == allTiles.get(posList[0]).south):
+                    numTrue +=1
+                else:
+                    numFalse +=1
+
+            if(allTiles.get(posList[2]) != None):
+                if(tile.south == allTiles.get(posList[2]).north):
+                    numTrue +=1
+                else:
+                    numFalse +=1
+
+            if(numTrue>1 and numFalse == 0):
+                returnTile = tile
+                numPosTile+=1
+
+            if(numPosTile>1):
+                break
+
+        if(numPosTile>1):
+            return "undetermined"
+        elif(returnTile != None):
+            return returnTile
+        else:
+            return "hole"
+
+def cover(tile):
+
+    x = tile.x
+    y = tile.y
+    posList = [(x,y+1),(x+1,y),(x,y-1),(x-1,y)]
+
+    for pos in posList:
+
+        try:
+            print(allTiles[pos])
+
+        except KeyError:
+            
+            if(pos in possibles):
+
+                tempTile = checkTile(pos)
+
+                print(tempTile)
+
+                print(type(tempTile))
+
+                print(type(tempTile)==tile)
+
+                if(type(tempTile) != str):
+                    print("Yessss")
+
+                    allTiles[pos] = tempTile
+
+            else:
+                possibles.add(pos)
+
+                print("This is an empty sqaure")
+
+tableSize = 10
+
+tileSet = [tile(1,0,2,1),tile(1,0,1,1),tile(0,0,0,0),tile(1,1,1,1),tile(1,2,1,2),tile(0,1,2,0)]
+
+allTiles = {}
+
+for x in range(100):
+        tile = copy.deepcopy(tileSet[random.randint(0, 5)])
+        rand = random.randint(1, tableSize -1)
+        tile.setTilePos(rand,rand)
+
+        allTiles[(rand,rand)] = tile
+
+print(allTiles)
+
+possibles = set()
+
+
+
+for tile in allTiles.values():
+    cover(tile)
+
+
+pygame.init()
+
+# Set up the drawing window
+screenDim = 1000
+screen = pygame.display.set_mode([screenDim, screenDim])
+
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    showTable(tableSize,screenDim)
+
+    for i in allTiles.values():
+        showTile(i,screenDim,tableSize)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
